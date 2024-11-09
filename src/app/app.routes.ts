e110d826a33a8f0ede1df019c46dc768eca6a663
gilvan-Borges <gilvan2022borges@gmail.com>
2024-11-09 17:59:58 -0300

    Implementação da funcionalidade de autenticação

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,27 +3,45 @@ import { CadastroClientesComponent } from './components/pages/cadastro-clientes/
 import { ConsultaClientesComponent } from './components/pages/consulta-clientes/consulta-clientes.component';
 import { EdicaoClientesComponent } from './components/pages/edicao-clientes/edicao-clientes.component';
 import { DashboardComponent } from './components/pages/dashboard/dashboard.component';
+import { AutenticarUsuarioComponent } from './components/pages/autenticar-usuario/autenticar-usuario.component';
+import { CriarUsuarioComponent } from './components/pages/criar-usuario/criar-usuario.component';
+import { LoginGuard } from './guards/login.guard';
+import { AuthGuard } from './guards/auth.guard';
 
 
 export const routes: Routes = [
     {
+        path:'pages/usuarios/autenticar',
+        component: AutenticarUsuarioComponent,
+        canActivate:[LoginGuard]
+    },
+    {
+        path:'pages/usuarios/criar',
+        component: CriarUsuarioComponent,
+        canActivate:[LoginGuard]
+    },
+    {
         path: 'pages/clientes/cadastro', //ROTA (URL)
-        component: CadastroClientesComponent
+        component: CadastroClientesComponent,
+        canActivate:[AuthGuard]
     },
     {
         path: 'pages/clientes/consulta', //ROTA (URL)
-        component: ConsultaClientesComponent
+        component: ConsultaClientesComponent,
+        canActivate:[AuthGuard]
     },
     {
         path: 'pages/clientes/edicao/:id', //ROTA (URL)
-        component: EdicaoClientesComponent
+        component: EdicaoClientesComponent,
+        canActivate:[AuthGuard]
     },
     {
         path: 'pages/clientes/dashboard', //ROTA (URL)
-        component: DashboardComponent
+        component: DashboardComponent,
+        canActivate:[AuthGuard]
     },
     {
         path: '', pathMatch: 'full', //ROTA raiz do projeto
-        redirectTo: '/pages/clientes/dashboard'
+        redirectTo: '/pages/usuarios/autenticar'
     }
 ];
--- src/app/components/layout/navbar/navbar.component.ts
+++ src/app/components/layout/navbar/navbar.component.ts
@@ -1,15 +1,47 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    CommonModule
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
 
+
+  //atributos
+  autenticado: boolean = false;
+  nomeUsuario: string = '';
+  perfilUsuario: string = '';
+
+
+  //função executada quando o componente é carregado
+  ngOnInit() {
+    //ler os dados do usuário contido na sessionstorage
+    var data = sessionStorage.getItem('usuario');
+    if(data) { //verificando se houveram dados obtidos
+      this.autenticado = true;
+      //ler os dados do usuário
+      var usuario = JSON.parse(data);
+      //armazenando o nome e o perfil do usuário
+      this.nomeUsuario = usuario.nome;
+      this.perfilUsuario = usuario.perfil;
+    }
+  }
+  //função para logout do usuário
+  logout(){
+    if(confirm('Deseja realmente sair do sistema?')){
+      //apagar os dados da session storage
+      sessionStorage.removeItem('usuario');
+        //redirecionando para a pagina de login
+      location.href= '/'
+    }
+  }
 }
